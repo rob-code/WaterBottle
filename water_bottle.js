fbe5426a8fabf7657c919582303428507294036d
Robert Brice <rob.a.brice@gmail.comC>
2017-04-04 13:51:54 +0100

    add drink method

--- water_bottle.js
+++ water_bottle.js
@@ -1,6 +1,21 @@
+
 var WaterBottle = function(){
   this.volume = 0;
+
+this.fill = function () {
+  return this.volume = 100;
 }
+
+this.drink = function () {
+  this.volume -= 10;
+}
+
+
+
+}
+
+
+
 
 
 module.exports = WaterBottle;
